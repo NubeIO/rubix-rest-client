6d17b5b8110cf336d1c2bb5835d1ed2361c7f185
NubeDev <aidan@envrioservices.com.au>
2021-08-10 17:16:35 +1000

    fix example

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ from config.load_config import get_config_host
 
 host = get_config_host().get("host")
 port = get_config_host().get("port")
+user = get_config_host().get("user")
+password = get_config_host().get("password")
 url = f"http://{host}:{port}"
 
 print(f"{host}:{port}")
@@ -12,8 +14,8 @@ print(f"{host}:{port}")
 # Setup the connection
 cx = rubix.Rubix(
     host=url,
-    user="admin",
-    password="N00BWires"
+    user=user,
+    password=password
 )
 
 d = discover.Discover(connection=cx)
