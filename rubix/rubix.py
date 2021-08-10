7e7bb9c4764128c66ba94f3cfed606eba2b3a94c
NubeDev <aidan@envrioservices.com.au>
2021-08-10 17:14:03 +1000

    setup of repo

---
+++ rubix/rubix.py
@@ -0,0 +1,48 @@
+import requests
+
+from rubix.utils.utils import Utils
+
+
+class Chirpstack:
+    def __init__(self,
+                 chirpstack_url=None,
+                 chirpstack_user=None,
+                 chirpstack_pass=None,
+                 jwt: str = None,
+                 connection_status: bool = None
+                 ):
+        self.chirpstack_url = chirpstack_url
+        self.chirpstack_user = chirpstack_user
+        self.chirpstack_pass = chirpstack_pass
+        self.jwt = jwt
+        self.connection_status = connection_status
+        self.connect()
+
+    def _authenticate(self):
+        auth_url = "%s/api/internal/login" % self.chirpstack_url
+        payload = {
+            "email": self.chirpstack_user,
+            "password": self.chirpstack_pass
+        }
+        auth_request = requests.post(
+            auth_url,
+            json=payload
+        )
+
+        self.connection_status = Utils.http_response_bool(auth_request)
+        auth_tok = auth_request.json()
+        self.jwt = auth_tok.get('jwt')
+        auth_header = {"Grpc-Metadata-Authorization": self.jwt}
+        return auth_header
+
+    def connect(self):
+        auth_header = self._authenticate()
+        is_connect = requests.Session()
+        is_connect.headers = auth_header
+        self.connection = is_connect
+
+    def get_jwt(self):
+        return self.jwt
+
+    def get_connection_status(self) -> bool:
+        return self.connection_status
---
+++ rubix/utils/utils.py
@@ -0,0 +1,56 @@
+import datetime
+import json
+import urllib.parse
+
+
+class Utils:
+
+    @classmethod
+    def encode_time(cls, ts):
+        return urllib.parse.quote_plus(ts.strftime('%Y-%m-%dT%H:%M:%SZ'))
+
+    @classmethod
+    def time_now(cls):
+        return datetime.datetime.now()
+
+    @classmethod
+    def day_subtract(cls, days):
+        now = Utils.time_now()
+        sub_days = datetime.timedelta(days=days)
+        return now - sub_days
+
+    @classmethod
+    def http_response(cls, res):
+        return_dict = {'result': 'success'}
+        if res.status_code != 200:
+            return_dict['result'] = "failure"
+            return_dict['message'] = json.loads(
+                res.content
+            )['message']
+            return return_dict
+        else:
+            return return_dict
+
+    @classmethod
+    def http_response_json(cls, res):
+        return_dict = {}
+        if res.status_code != 200:
+            return_dict['result'] = "failure"
+            return_dict['message'] = json.loads(
+                res.content
+            )['message']
+            return return_dict
+        else:
+            return res.json()
+
+    @classmethod
+    def http_response_bool(cls, res):
+        return_dict = {'result': 'success'}
+        if res.status_code != 200:
+            return_dict['result'] = "failure"
+            return_dict['message'] = json.loads(
+                res.content
+            )['message']
+            return False
+        else:
+            return True
