c4e7523cef6b9f4d1255c54b595d19686c3a5990
NubeDev <aidan@envrioservices.com.au>
2021-08-20 07:00:35 +1000

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rubix-rest-client',
+    version='',
+    packages=['rubix', 'rubix.utils'],
+    url='https://github.com/NubeIO/rubix-rest-client-python',
+    license='',
+    author='ap',
+    author_email='ap@nube-io.com',
+    description='nube api cli'
+)
